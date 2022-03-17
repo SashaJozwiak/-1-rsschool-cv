60d8bc19bdeb61ceb63670019161b5417d1c3a9b
Jozwiak <wmdz88@gmail.com>
2022-03-17 12:50:22 +0300

    fix: text in profile

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 document.querySelector("#btn").onclick = function () {
     const code = document.getElementById("code");
-    const showcode = 'code.innerHTML += `${showcode}`;\nbtn.remove();\n};';
-    code.innerHTML += `${showcode}`;
+    const showСode = 'code.innerHTML += `${showСode}`;\nbtn.remove();\n};';
+    code.innerHTML += `${showСode}`;
     btn.remove()
 };
